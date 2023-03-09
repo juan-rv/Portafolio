e3e1792c038ccacb578ba781b6572e005dcc8c73
juan-rv <juanrodriguez9502@gmail.com>
2023-03-09 13:14:46 -0500

    services

---
+++ src/api/services.js
@@ -0,0 +1,33 @@
+export const services = [
+    {
+        'id': '1',
+        'name': 'Diseño y Desarrollo Web.',
+        'resume' : {
+            'uno': 'Diseño Web: Creación de diseños personalizados y unicos para sitios web, que reflejen la identidad de la marca',
+             'dos': 'Desarrollo Web: Creación de aplicaciones web personalizadas, que satisfagan las necesidade específicas de los clientes.',
+             'tres': 'Creación de Sitios Web desde 0: Ofrezco servicios completos de creación de sitios web desde cero, desde la planificación, hasta la implementación.',
+             'cuatro': 'Optimización de motores de busqueda (SEO): Ofrezco servicios de SEO para ayudar a los clientes a aumentar su visibilidad en línea.',
+             'cinco': 'Mantenimiento y Soporte: Mantenimiento y soporte garantizando que los sitios web de los clientes estén actualizados y funcionando sin problemas.'
+        } 
+               
+    },
+
+    {
+        'id': '2',
+        'name': 'Consultoría IT.',
+        'resume': {
+            'uno': 'Asesoramiento sobre gestion de proyectos para un equipo de desarrollo de software.',
+            'dos' : 'Planificacion de proyectos TI para su empresa.'
+        }
+    },
+    {
+        'id': '3',
+        'name': 'Demás Servicios.',
+        'resume': {
+            'uno': 'Tablas dinámicas en excel con macros de acuerdo a las necesidades de su empresa o comercio.',
+            'dos':'Diseño y publicación del Brochure empresarial.'
+        }
+    }
+]
+
+//  
--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { services } from "../api/services";
+import "../styles/servicios.css";
 
 const Servicios = () => {
   return (
-    <div>
-      <h1>Pagina de servicios</h1>
+    <div className="services_contain">
+      <div className="services">
+        <div className="text_services">
+          <h2>
+            Bienvenido a mi sección de servicios. Como desarrollador web,
+            ofrezco una amplia gama de servicios para ayudar a las empresas y
+            organizaciones a alcanzar sus objetivos en línea.
+          </h2>
+          <h3>
+            Desde el diseño y desarrollo de sitios web personalizados hasta la
+            consultoría en gestión de proyectos, estoy aquí para ayudarte a
+            lograr el éxito en línea. A continuación, te presento algunos de los
+            servicios más populares que ofrezco. Si tienes alguna pregunta o
+            estás interesado en alguno de ellos, no dudes en ponerte en contacto
+            conmigo. ¡Estoy aquí para ayudarte!
+          </h3>
+        </div>
+      </div>
+
+      <div className="contain_card">
+        {services.map((service) => {
+          console.log(service.servicios);
+          return (
+            <div key={service.id} className="cards">
+              <h2>{service.name}</h2>
+              <p>{service.resume.uno}</p>
+              <p>{service.resume.dos}</p>
+              <p>{service.resume.tres}</p>
+              <p>{service.resume.cuatro}</p>
+              <p>{service.resume.cinco}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
