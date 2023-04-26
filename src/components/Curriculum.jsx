1732b3eb4b30815b9023798b62170fc5bb6a769a
juan-rv <juanrodriguez9502@gmail.com>
2023-04-26 18:18:24 -0500

    curriculum full responsive design

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import cv from "../assets/cv.jpg";
+import cvDownload from "../assets/CV.pdf";
 import "../styles/curriculum.css";
 
 const Curriculum = () => {
   return (
-    <div className="curriculum">
-      <div className="logo"></div>
-      <h1>Pagina de Curriculum</h1>
+    <div className="content_cv">
+      <br />
+      <div className="title_curriculum">
+        <h1>Curriculum</h1>
+      </div>
+
+      <div className="img_cv">
+        <img src={cv} alt="cv" />
+      </div>
+      <div className="download">
+        <a href={cvDownload} download>
+          <button>Download CV</button>
+        </a>
+      </div>
     </div>
   );
 };
